cb1e7030fc2cfa2365a68401c55729fe2d24feff
Paul M. Yncaluque <yncaluquedev@gmail.com>
2023-03-26 21:24:16 -0500

    Install ng-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+// Ngrx Store
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
+
+// ng-boostrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Components
+import { AppComponent } from './app.component';
 import { SalePageComponent } from './pages/sale-page/sale-page.component';
 
 @NgModule({
@@ -16,6 +23,8 @@ import { SalePageComponent } from './pages/sale-page/sale-page.component';
     StoreModule.forRoot({}, {}),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
     EffectsModule.forRoot([]),
+    BrowserAnimationsModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
